de0d8f41c9fbd9b49c0a8f653d1a94c444eaab14
SamHammersley <sam.gonsalves@protonmail.com>
2021-11-04 23:21:58 +0000

    Subroutine to read from standard input, with option to subtract 48 (the ascii value of 0) for numbers. Given as a parameter (flag) in RDI register

--- include/io_util.asm
+++ include/io_util.asm
@@ -1,27 +1,24 @@
-global _start
-
 section .data
-  null_char:            equ 0
-  new_line_char:        equ 10
-  max_buffer_size:      equ 128
-  default_buffer_size:  equ 128
-  message:              db  "Hello world"
+  new_line_char:        equ  10
+  default_buffer_size:  equ  64
+  read_int_id:          equ  0
 
 section .bss
-  buffer:               resb  default_buffer_size
+  char_buffer:          resb default_buffer_size
 
 section .text
-_start:
-  mov r10, 0        ; buffer size
-  mov rsi, buffer   ; buffer write pos
 
-read_char_stdin:
-  cmp r10, max_buffer_size
-  je print_buffer
+; expects a 0 in rdi if each byte of the input correct by 48, the ascii offset.
+read_chars:
+  pop r8
+  push rdi
+  mov r10, 0                 ; buffer size
+  mov rsi, char_buffer       ; buffer write pos
 
-  mov rax, 0       ; syscall id (read)
-  mov rdi, 0       ; file descriptor (stdin)
-  mov rdx, 1       ; 1 byte
+next_char:
+  mov rax, 0                 ; syscall id (read)
+  mov rdi, 0                 ; file descriptor (stdin)
+  mov rdx, 1                 ; 1 byte
   syscall
 
   mov r9, 0
@@ -29,19 +26,20 @@ read_char_stdin:
   inc rsi
   inc r10
 
-  cmp r9, 10
-  jne read_char_stdin
+  cmp r9, new_line_char
+  je end_of_input
 
-  push buffer
+  pop rdi
+  cmp r11, read_int_id
+  jne jump_to_next
+  
+  add byte [rsi-1], 1
 
-print_buffer: ; this is just for testing.
-  mov rax, 1       ; syscall id (read)
-  mov rdi, 1       ; file descriptor (stdout)
-  mov rsi, buffer  ;buffer  ; the buffer
-  mov rdx, r10
-  syscall
+jump_to_next: 
+  jmp next_char
 
-  ; terminate below
-  mov rax, 60
-  mov rdi, 0
-  syscall
+end_of_input:
+  push char_buffer
+  push r10
+  push r8
+  ret
