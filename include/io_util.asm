7de2e8e0dbb2bccf1b3878e15b08423eef9d4842
SamHammersley <sam.gonsalves@protonmail.com>
2021-11-12 00:24:20 +0000

    Change io_util readchars function to use own stack frame and return values in registers

--- include/io_util.asm
+++ include/io_util.asm
@@ -2,44 +2,52 @@ section .data
   new_line_char:        equ  10
   default_buffer_size:  equ  64
   read_int_id:          equ  0
+  ascii_digit_offset:	equ  48
 
 section .bss
   char_buffer:          resb default_buffer_size
 
 section .text
-
-; expects a 0 in rdi if each byte of the input correct by 48, the ascii offset.
+; reads input from standard input, byte by byte.
+; expects a 0 in rdi if each byte of the input
+;   should be correct by 48, the ascii offset.
 read_chars:
-  pop r8
-  push rdi
+  push rbp
+  mov rbp, rsp
+  sub rsp, 8
+
+  mov [rbp - 8], rdi	     ; store rdi in free stack space. 
   mov r10, 0                 ; buffer size
   mov rsi, char_buffer       ; buffer write pos
 
+; todo: maybe take file descriptor as parameter, 
+; this would depend on whether, the language should
+; support reading from files. (not a given)
 next_char:
   mov rax, 0                 ; syscall id (read)
   mov rdi, 0                 ; file descriptor (stdin)
   mov rdx, 1                 ; 1 byte
   syscall
 
-  mov r9, 0
-  mov r9b, byte [rsi]
   inc rsi
   inc r10
 
-  cmp r9, new_line_char
+  cmp byte [rsi - 1], new_line_char
   je end_of_input
 
-  pop rdi
-  cmp r11, read_int_id
+  cmp byte [rbp - 8], read_int_id
   jne jump_to_next
   
-  add byte [rsi-1], 1
+  sub byte [rsi - 1], ascii_digit_offset
 
 jump_to_next: 
   jmp next_char
 
 end_of_input:
-  push char_buffer
-  push r10
-  push r8
+  mov rsp, rbp
+  pop rbp
+
+; returned values in rax and rcx.
+  mov rax, char_buffer
+  mov rcx, r10
   ret
